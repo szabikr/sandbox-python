c5cbf430aef4dfc16112d97da2b42ad02db11074
szabikr <szabikr.dev@gmail.com>
2022-01-05 21:34:52 +0000

    xerox-pkg: copy requested phrase to clipboard

--- xerox-pkg/mclip.py
+++ xerox-pkg/mclip.py
@@ -1,4 +1,10 @@
-import sys
+TEXT = {
+  'agree': """Yes, I agree. That sounds fine to me.""",
+  'busy': """Sorry, can we do this later this week or next week?""",
+  'upsell': """Would you consider making this a monthly donation?"""
+}
+
+import sys, xerox
 
 if len(sys.argv) < 2:
   print('Usage: python3 mclip.py [keyphrase] - copy phrase text')
@@ -6,4 +12,8 @@ if len(sys.argv) < 2:
 
 keyphrase = sys.argv[1]
 
-print(keyphrase)
+if keyphrase in TEXT:
+  xerox.copy(TEXT[keyphrase])
+  print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+  print('There is no text for ' + keyphrase)
