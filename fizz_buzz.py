a2b9a5986cfe58ef06085f646b6fb530067412e8
szabikr <szabikr.dev@gmail.com>
2022-01-07 22:17:06 +0000

    fizz_buzz: Making algorithm a bit more functional

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -6,18 +6,21 @@ if len(sys.argv) < 2:
 
 count = sys.argv[1]
 
+def fizzBuzz(number):
+  if number % 3 == 0 and number % 5 == 0:
+    return 'fizzbuzz'
+    
+  if number % 3 == 0:
+    return 'fizz'
+
+  if number % 5 == 0:
+    return 'buzz'
+    
+  return number
+
 # generate a list with size count
 for number in range(int(count) + 1):
   if number == 0:
     continue
 
-  if number % 3 == 0 and number % 5 == 0:
-    print('fizzbuzz')
-  else:
-    if number % 3 == 0:
-      print('fizz')
-    else:
-      if number % 5 == 0:
-        print('buzz')
-      else:
-        print(number)
+  print(fizzBuzz(number))
